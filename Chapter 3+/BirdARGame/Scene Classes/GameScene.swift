67da728e8604aa7a8a9c5efb047006d68c9414b8
BergQuester <daniel.bergquist@gmail.com>
2019-04-01 15:41:13 -0600

    Spawn birds method

--- Chapter 3+/BirdARGame/Scene Classes/GameScene.swift
+++ Chapter 3+/BirdARGame/Scene Classes/GameScene.swift
@@ -17,8 +17,23 @@ class GameScene: SKScene {
     var numberOfBirds = 10
     var timerLabel:SKLabelNode!
     var counterLabel:SKLabelNode!
-    
 
+    func spawnBirds() {
+        guard let sceneView = self.view as? ARSKView else {
+            return
+        }
+
+        if let currentFrame = sceneView.session.currentFrame {
+            var translation = matrix_identity_float4x4
+
+            translation.columns.3.x = randomPosition(lowerBound: -0.2, upperBound: 0.2)
+            translation.columns.3.y = randomPosition(lowerBound: -0.2, upperBound: 0.2)
+            translation.columns.3.z = randomPosition(lowerBound: -0.5, upperBound: 0.2)
+
+            let transform = simd_mul(currentFrame.camera.transform, translation)
+
+            let anchor = ARAnchor(transform: transform)
+            sceneView.session.add(anchor: anchor)
+        }
+    }
 }
-
-
